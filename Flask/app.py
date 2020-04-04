2f089328e451a0091ce4e306c4a47d74333b5aaf
Victor Rodriguez Boyero <victorrb.telefonica@gmail.com>
2020-04-04 14:18:16 +0200

    Heatmap
    Heatmap layout

--- Flask/app.py
+++ Flask/app.py
@@ -1,4 +1,5 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,jsonify
+import json
 
 from flask_bootstrap import Bootstrap
 app = Flask(__name__)
@@ -9,6 +10,30 @@ Bootstrap(app)
 def index():    
     return render_template("index.html")
 
+@app.route('/mapa',methods=['GET', 'POST'])
+def mapa(): 
+    return render_template("Mapa.html" )
+
+@app.route('/mapa_simple',methods=['GET', 'POST'])
+def mapa_simple(): 
+    return render_template("mapa_simple.html" )
+@app.route('/longitudes',methods=['GET', 'POST'])
+def Longitudes():
+  
+    puntosArray = []
+    # Using readlines() 
+    file1 = open('Longitudes.txt', 'r') 
+    Lines = file1.readlines()  
+    # Strips the newline character 
+    for line in Lines:          
+        line = line.rstrip('\n')
+        puntoObj = {}        
+        latitud = "40.537685"
+        longitud = str(line)
+        puntoObj['Latitud'] = latitud
+        puntoObj['Longitud'] = longitud    
+        puntosArray.append(puntoObj)  
+    return jsonify(puntosArray)
 
 
 
