472abf02386f192839984fd38b1b9070e516da6b
Victor Rodriguez Boyero <victorrb.telefonica@gmail.com>
2020-04-05 12:52:42 +0200

    historical data
    get historical temperature data

--- Flask/app.py
+++ Flask/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, url_for, request, redirect, jsonify, config
 import json
 import requests
-
+import numpy as np
 from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 
@@ -91,10 +91,71 @@ def Getlocation():
         tempArray.append(data['temp'])             
         
     print(tempArray)
+    print(temperature_data(provincia))
+
+
 
 
     return 'ok'
     
+
+
+
+
+
+
+  
+def temperature_data(name):
+    r, station_code = get_stations(provincia = name.upper())
+
+    api_key = api_key = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJwYWJsbzE2Mjk4QGhvdG1haWwuY29tIiwianRpIjoiZWY3MTZmYTUtZjdjNy00MmY0LWI0ZDEtN2RmOTAyMzA5M2FlIiwiaXNzIjoiQUVNRVQiLCJpYXQiOjE1ODU1NjA3MDUsInVzZXJJZCI6ImVmNzE2ZmE1LWY3YzctNDJmNC1iNGQxLTdkZjkwMjMwOTNhZSIsInJvbGUiOiIifQ.Zn3YOUpHQYrcf4woFBZP0vgzSmxJjlDRFqu9rQfNxwI"
+    base_url = 'https://opendata.aemet.es/opendata/api/valores/climatologicos/diarios/datos/fechaini/2020-01-01T00%3A00%3A00UTC/fechafin/2020-04-03T00%3A00%3A00UTC/estacion/{}'.format(station_code)
+
+    params = {
+        'api_key': api_key
+    }
+    headers = {
+        'cache-control': "no-cache"
+    }
+
+    response = requests.request('GET', base_url, params=params, headers=headers)
+    print("respuesta datos provincia")
+    print(response)
+    response_url = response.json()['datos']
+
+    data = requests.request('GET', response_url)
+    
+    simple_results = np.array([float(value['tmed'].replace(',','.')) for value in data.json()])
+
+    results = {}
+    for value in data.json():
+        results[value['fecha']] = float(value['tmed'].replace(',','.'))
+
+    return results, simple_results
+
+def get_stations(provincia):
+    api_key = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJwYWJsbzE2Mjk4QGhvdG1haWwuY29tIiwianRpIjoiZWY3MTZmYTUtZjdjNy00MmY0LWI0ZDEtN2RmOTAyMzA5M2FlIiwiaXNzIjoiQUVNRVQiLCJpYXQiOjE1ODU1NjA3MDUsInVzZXJJZCI6ImVmNzE2ZmE1LWY3YzctNDJmNC1iNGQxLTdkZjkwMjMwOTNhZSIsInJvbGUiOiIifQ.Zn3YOUpHQYrcf4woFBZP0vgzSmxJjlDRFqu9rQfNxwI"
+    base_url = 'https://opendata.aemet.es/opendata/api/valores/climatologicos/inventarioestaciones/todasestaciones'
+
+    params = {
+        'api_key': api_key
+    }
+    headers = {
+        'cache-control': "no-cache"
+    }
+
+    response = requests.request('GET', base_url, params=params, headers=headers)
+    print("imprimiendo respuesta estaciones provincia")
+    print(response)
+    response_url = response.json()['datos']
+
+    data = requests.request('GET', response_url)
+
+    print("imprimiendo data")
+    print(data)
+    results = [result for result in data.json() if result['provincia'] == provincia]
+    
+    return results, results[0]['indicativo']
 
     
 
